f345f498972b34891464e5496912e5d6dc911ec9
Raquelgloria <rakivaz16@gmail.com>
2023-11-25 14:23:01 -0600

    practica 13

---
+++ miApp-2011258/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'miApp-2011258',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
